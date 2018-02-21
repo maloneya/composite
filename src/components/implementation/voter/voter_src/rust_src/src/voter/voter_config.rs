c67246e7bbc3d161ad557b8d73558bd969cb1f63
Alex Maloney <maloney.a12@gmail.com>
2018-02-21 15:20:06 -0500

    Cleanup feedback from PR311
    Rename reps - replicas
    move constants to voter config file
    clean up a few data races

---
+++ src/components/implementation/voter/voter_src/rust_src/src/voter/voter_config.rs
@@ -0,0 +1,14 @@
+//maximum number of components that can be booted through the voter
+pub const MAX_COMPS: usize = 2;
+//RR priority of the replica threads
+pub const REP_PRIO: u32 = 5;
+//RR priority of the voter thread while voting
+pub const VOTE_PRIO: u32 = 1;
+//maximum number of consecutive scheduler quantoms we allow a replica to
+//continue to process after other components in its component have reached
+//consensus
+pub const MAX_INCONCLUSIVE: u8 = 10;
+//maximum number of replica composite components per voter component
+pub const MAX_REPS: usize = 3;
+//size of the data buffer passed between application and server components.
+pub const BUFF_SIZE: usize = 16;
