0d417a3a1361a7bb85ec532b051bc5621b017a5f
Alex Maloney <maloney.a12@gmail.com>
2018-04-14 16:58:34 -0700

    Integrate memmgr api into lib_composite
    rust can now create and map shared pages safely though this api

--- lib_composite/src/sys/mod.rs
+++ lib_composite/src/sys/mod.rs
@@ -1,6 +1,7 @@
 // TODO: Use bindgen to create this bindings (doing in manually is brittle)
 pub mod cos_defkernel_api;
 pub mod cos_kernel_api;
+pub mod memmgr;
 pub mod sl;
 pub mod sl_lock;
 pub mod types;
