95a4415c969fa928ab527390802b4eca85f758db
Alex Maloney <maloney.a12@gmail.com>
2018-04-15 14:36:12 -0700

    update test_replica to use new interface

--- src/components/implementation/no_interface/test_replica/replica.c
+++ src/components/implementation/no_interface/test_replica/replica.c
@@ -2,24 +2,23 @@
 #include <voter.h>
 #include <cos_types.h>
 #include <cobj_format.h>
-#include "../../interface/capmgr/memmgr.h"
 
-void make_sys_call(vaddr_t shdmem_addr, int shdmem_id) {
-	static int j = 0;
-    for (int i = 0; i < 5; i++) {
-    	*(int *)(shdmem_addr+i) = j++;
-    }
+void make_sys_call() {
+	int fd = 3;
+	int size = 5;
+	int data[size];
+	for (int i = 0; i < size; i++) 
+		data[i] = i;
+
     printc("Replica making syscall\n");
-	voter_write(shdmem_id,5);
-	//check for data sent back from the voter
-	//TODO build real return interface
-	printc("replica got: %d\n", *(char*)shdmem_addr);
+	int ret = voter_write(fd,data,size);
+	printc("replica got: %d\n", ret);
 }
 
-void do_work(vaddr_t shdmem_addr, int shdmem_id) {
+void do_work() {
 	for (int i = 0;;i++) {
 		if (i == 100) {
-			make_sys_call(shdmem_addr,shdmem_id);
+			make_sys_call();
 			i = 0;
 		}
 	}
@@ -27,16 +26,10 @@ void do_work(vaddr_t shdmem_addr, int shdmem_id) {
 
 void cos_init(void)
 {
-	vaddr_t shdmem_addr;
-	int shdmem_id;
-
 	printc("Replica booted\n");
 
-	shdmem_id = memmgr_shared_page_alloc(&shdmem_addr);
-    assert(shdmem_id > -1 && shdmem_addr > 0);
-
-	replica_done_initializing(shdmem_id);
-	do_work(shdmem_addr, shdmem_id);
+	replica_done_initializing();
+	do_work();
 
 	return;
 }
